fd6a453e3e9b612dc258bf0d909b0831f8d0f240
Vasil Pavlov <vasilpavlov@Vasils-MacBook-Pro.local>
2019-11-21 12:42:52 +0200

    Extract nav items in constants

---
+++ client/src/components/navigation/Constants.js
@@ -0,0 +1,8 @@
+export const NAV_ITEMS = [
+	{ navItem: 'Projects', iconName: 'dashboard' },
+	{ navItem: 'Clients', iconName: 'group' },
+	{ navItem: 'Employees', iconName: 'user-secret' },
+	{ navItem: 'Financials', iconName: 'money' },
+	{ navItem: 'Documents', iconName: 'book' },
+	{ navItem: 'Settings', iconName: 'cog' }
+];
--- client/src/components/navigation/SideNavigation.js
+++ client/src/components/navigation/SideNavigation.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import { Toggle, Sidenav, Nav, Dropdown, Icon } from 'rsuite';
 
+import { NAV_ITEMS } from './Constants';
+
 import 'rsuite/dist/styles/rsuite-default.css';
 
 class SideNavigation extends Component {
@@ -8,21 +10,35 @@ class SideNavigation extends Component {
 		super();
 		this.state = {
 			expanded: false,
-			activeKey: '1'
+			activeKey: '0'
 		};
+
 		this.handleToggle = this.handleToggle.bind(this);
 		this.handleSelect = this.handleSelect.bind(this);
 	}
+
 	handleToggle() {
 		this.setState({
 			expanded: !this.state.expanded
 		});
 	}
+
 	handleSelect(eventKey) {
 		this.setState({
 			activeKey: eventKey
 		});
 	}
+
+	renderNavItems = () => {
+		return NAV_ITEMS.map((item, index) => {
+			return (
+				<Nav.Item eventKey={index} icon={<Icon icon={item.iconName} />}>
+					{item.navItem}
+				</Nav.Item>
+			);
+		});
+	};
+
 	render() {
 		const { expanded } = this.state;
 
@@ -43,26 +59,7 @@ class SideNavigation extends Component {
 						style={{ margin: '3% 9%' }}
 					/>
 					<Sidenav.Body>
-						<Nav>
-							<Nav.Item eventKey="1" icon={<Icon icon="dashboard" />}>
-								Projects
-							</Nav.Item>
-							<Nav.Item eventKey="2" icon={<Icon icon="group" />}>
-								Clients
-							</Nav.Item>
-							<Nav.Item eventKey="3" icon={<Icon icon="user-secret" />}>
-								Employees
-							</Nav.Item>
-							<Nav.Item eventKey="4" icon={<Icon icon="money" />}>
-								Financials
-							</Nav.Item>
-							<Nav.Item eventKey="5" icon={<Icon icon="book" />}>
-								Documents
-							</Nav.Item>
-							<Nav.Item eventKey="5" icon={<Icon icon="cog" />}>
-								Settings
-							</Nav.Item>
-						</Nav>
+						<Nav>{this.renderNavItems()}</Nav>
 					</Sidenav.Body>
 				</Sidenav>
 			</div>
