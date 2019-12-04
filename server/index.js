29962b4b6343a3122e3a1e926e56d6747617331e
boyandonchev <boyandon@gmail.com>
2019-12-04 09:45:14 +0200

    Swagger update

--- server/index.js
+++ server/index.js
@@ -3,7 +3,11 @@ const app = express();
 const sign = require('./routes/sign.js');
 const clients = require('./routes/clients.js');
 
-
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 app.use('/sign',sign);
 app.use('/clients',clients);
 
