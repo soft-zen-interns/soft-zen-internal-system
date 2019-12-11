1012cc4662ff2b2541d1b446378077a8dbeb32ca
Alexander <azlibe1995@gmail.com>
2019-12-11 17:13:05 +0200

    Setup example tests

---
+++ server/test/example.test.js
@@ -0,0 +1,8 @@
+const assert = require('assert')
+
+describe('Example test', () => {
+  it('valToTest should be \'test me\'', function() {
+    const valToTest = 'test me'
+    assert(valToTest === 'test me')
+  });
+})
